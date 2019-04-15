3cb12ca2ea9cb46f398b1a794920ffd8149889ad
Nick Pettazzoni <pettazz@gmail.com>
2019-04-15 00:40:12 -0400

    use color-thief to try to make the logo always readable

--- assets/js/common.js
+++ assets/js/common.js
@@ -8,4 +8,24 @@ $(document).ready(function(){
   });
   retinajs();
 
+  var headliner = $('.headliner > img');
+
+  if(headliner.length > 0){
+    var colorThief = new ColorThief(),
+        dominantColor = colorThief.getColor(headliner[0], 5),
+        rgbStringInverted = 'rgb(' + 
+                    (255 - dominantColor[0]) + ', ' +
+                    (255 - dominantColor[1]) + ', ' +
+                    (255 - dominantColor[2]) + ')',
+        rgbStringDominant = 'rgba(' + 
+                    dominantColor[0] + ', ' +
+                    dominantColor[1] + ', ' +
+                    dominantColor[2] + ', 0.8)';
+
+    console.log(rgbStringDominant);
+    console.log(rgbStringInverted);
+    $('#wnv-logo .chunk').css('fill', rgbStringInverted);
+    $('#wnv-logo').css('filter', 'drop-shadow(0px 0px 10px ' + rgbStringDominant + ')');
+  }
+
 });
