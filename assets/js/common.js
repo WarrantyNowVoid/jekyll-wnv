ec255fd3177cae6457a40459222f3783690d2e0a
Nick Pettazzoni <pettazz@gmail.com>
2019-04-22 00:51:22 -0400

    more better layout stuff

--- assets/js/common.js
+++ assets/js/common.js
@@ -16,18 +16,22 @@ $(document).ready(function(){
 
   if(image){
 
-    var vibrant = new Vibrant(image),
+    var vibrant = new Vibrant(image, 64, 1),
         swatches = vibrant.swatches(),
-        rgbStringDominant = toRGBAString(swatches['Vibrant'].getRgb(), 1),
-        rgbStringInverted = toRGBAString(swatches['DarkVibrant'].getRgb(), 1),
-        rgbStringAnother = toRGBAString(swatches['Muted'].getRgb(), 1);
+        rgbVibrant = toRGBAString(swatches['Vibrant'].getRgb(), 1),
+        rgbDark = toRGBAString(swatches['DarkVibrant'].getRgb(), 1);
 
-    $('#wnv-logo .chunk').css('fill', rgbStringInverted);
-    $('.title-background-bar').css('background-color', rgbStringDominant);
-    // $('.title-wrap h1 a').css('color', '#fff');
-    $('.title-wrap h1 a').css('text-shadow', '2px 2px 0px ' + rgbStringInverted);
-    $('.title-wrap p').css('text-shadow', '1px 1px 0px ' + rgbStringInverted);
-    $('#wnv-logo').css('filter', 'drop-shadow(2px 2px 0px ' + rgbStringDominant + ')');
+    $('#wnv-logo .chunk').css('fill', rgbVibrant);
+    $('#wnv-logo').css('filter', 'drop-shadow(2px 2px 0px ' + rgbDark + ')');
+
+    $('.title-background-bar').css('background-color', rgbDark);
+
+    $('.comic-sub a').css('color', rgbVibrant);
+
+    $('.post-content .tags').css('background-color', rgbVibrant);
+    $('.post-content .related').css('background-color', rgbDark);
+    // $('.title-wrap h1 a').css('text-shadow', '2px 2px 0px ' + rgbDark);
+    // $('.title-wrap p').css('text-shadow', '1px 1px 0px ' + rgbDark);
   }
 
 });
